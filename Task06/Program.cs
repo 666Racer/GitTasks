43ef5d3a023754db4bc2bafbe7cc23f5e3eeb6f2
Misha <misha.hanzhin@gmail.com>
2023-04-22 12:35:11 +0500

    Задача 6 на проверку чётности числа

---
+++ Task06/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) num *= -1;
+int checkeven = num % 2;
+if (checkeven == 1)
+    {
+    Console.WriteLine($"Число {num} нечётное");
+    }
+else if (checkeven == 0)
+    {
+    Console.WriteLine($"Число {num} чётное");
+    }
