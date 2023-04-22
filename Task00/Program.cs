54b3663377974197fae25ea76386dca34d2b2fcd
Misha <misha.hanzhin@gmail.com>
2023-04-22 13:04:08 +0500

    Изменения в программе в папке Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -5,6 +5,7 @@
 4 -> 16
 -3 -> 9
 -7 -> 49*/
+Console.WriteLine("Введите целое число"); //не более 6 знаков
 int number = Convert.ToInt32(Console.ReadLine()); // "7"
-int sqr = number*number;
-Console.WriteLine(sqr);
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
