70f40859c04700287bde9587cf1cb1c8081cb16d
Misha <misha.hanzhin@gmail.com>
2023-04-24 23:02:24 +0500

    Добавлено решение методом (функцией)

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -4,12 +4,22 @@
 78 -> 8
 12-> 2
 85 -> 8*/
-int num = new Random().Next(10, 100); //(10. (99 + 1))
-Console.WriteLine($"Случайное число из отрезка [10, 99] -> {num}");
+int number = new Random().Next(10, 100); //(10. (99 + 1))
+Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
 
-int digit1 = num / 10;
-int digit2 = num % 10;
+/*int digit1 = number / 10;
+int digit2 = number % 10;
 
 if (digit1 > digit2) Console.WriteLine($"Наибольшая цифра числа {num} это {digit1}");
-else Console.WriteLine($"Наибольшая цифра числа {num} это {digit2}");
+else Console.WriteLine($"Наибольшая цифра числа {num} это {digit2}");*/
 //int maxDigit = digit 1 > digit2 ? digit1 : digit2
+int result = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа {number} это {result}");
+
+int MaxDigit (int num) //num = number
+{
+    int digit1 = num / 10;
+    int digit2 = num % 10;
+    int result = digit1 > digit2 ? digit1 : digit2;
+    return result;
+}//реализация метода (функции)
