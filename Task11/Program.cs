c639e895b6407c77952e44c42a6d32f7390868a8
Misha <misha.hanzhin@gmail.com>
2023-04-24 23:21:12 +0500

    Задача на удаление второй цифры в трёхзначном числе

---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+11. Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого
+числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int number = new Random().Next(100, 1000); //(10. (999 + 1))
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+
+int result = DeleteDigit2 (number);
+Console.WriteLine($"Наибольшая цифра числа {number} это {result}");
+
+int DeleteDigit2 (int num) //num = number
+{
+    int digit1 = num / 100; //получаем первую цифру
+    int digit3 = num % 10;  //получаем третью цифру
+    int result = digit1 * 10 + digit3; //получаем число без второй цифры
+    return result; //возвращаем результат
+}
