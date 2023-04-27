7c013ba20eb84df875cb6c178a5d52c1b0b1cb2d
Misha <misha.hanzhin@gmail.com>
2023-04-27 21:11:24 +0500

    Задача на проверку выходного по номеру дня недели

---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 1 && number <= 7)
+{
+    bool result = DayOff (number);
+    Console.WriteLine (result ? "это выходной" : "это не выходной");
+}
+else
+{
+    Console.WriteLine ("Не является номером дня недели");
+}
+
+bool DayOff (int num)
+{
+    return num == 6 || num == 7;
+}
