7500809d0f2c1a11919770dea547ab004496ed20
Misha <misha.hanzhin@gmail.com>
2023-04-30 13:03:46 +0500

    Задача на координаты по четверти

---
+++ Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 18: Напишите программу, которая по
+заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y).
+*/
+Console.WriteLine ("Введите номер четверти");
+string quarter = Console.ReadLine();
+string coordinate = CoordinateQuarter (quarter);
+
+string result = coordinate != null
+        ? $"В {quarter} четверти находятся точки в диапазоне {coordinate}"
+        : "Некорректно указана четверть";
+Console.WriteLine (result);
+
+string CoordinateQuarter (string quart)
+{
+    if (quart == "1") return "x = [0;+oo); y = [0;+oo)";
+    if (quart == "2") return "x = (-oo;0]; y = [0;+oo)";
+    if (quart == "3") return "x = (-oo;0]; y = (-oo;0]";
+    if (quart == "4") return "x = [0;+oo); y = (-oo;0]";
+    return null;
+}
