568e8f1245db4e05e9dac5bf145bc680b9249acc
Misha <misha.hanzhin@gmail.com>
2023-05-03 20:26:26 +0500

    Задача на таблицу кубов

---
+++ Task23/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125
+*/
+Console.WriteLine ("Введите целое число N");
+int numberN = Convert.ToInt32(Console.ReadLine()); //хотя в условии не говорится, что число N - целое, как и положительное, но с double почему-то не работает
+
+Cube (numberN);
+
+void Cube (int num)
+{
+    if (num > 0)
+    {
+    int count = 1;
+    while (count <= num)
+        {
+        Console.WriteLine($"{count,3} {count*count*count,5} ");
+        count +=1;
+        }
+    }
+    else if (num < 0)
+    {
+    int count = 1;
+    while (count >= num)
+        {
+        Console.WriteLine($"{count,3} {count*count*count,5} ");
+        count -=1;
+        }
+    }
+    else if (num < 1 && num > -1)
+        {
+        Console.WriteLine("Число по модулю меньше 1");
+        }
+}
