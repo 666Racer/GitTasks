8a95e72d4877e4ac5661457dc22cec6c5351e949
Misha <misha.hanzhin@gmail.com>
2023-05-19 00:35:47 +0500

    Задача на заполнение массива 0 и 1

---
+++ Task30/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 30: Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int [] arr = new int [8];
+
+FillArray (arr);
+PrintArray (arr);
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        
+        Console.Write($"{array[i]},");
+        
+    }
+    Console.Write($"{array[array.Length-1]}");
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void FillArray (int[] array)
+{
+    var rnd = new Random();//чтобы каждый раз не вызывать в цикле random и не получать одинаковые значения
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0,2);//здесь только next
+    }
+}
