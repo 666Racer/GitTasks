f9db48ed34a8ae7cb9fcc4a7b3842c8a4f31fa19
Misha <misha.hanzhin@gmail.com>
2023-05-27 17:44:49 +0500

    Задача на дублирование массива

---
+++ Task45/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 45: Напишите программу, которая будет создавать
+копию заданного массива с помощью поэлементного
+копирования.
+*/
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = " ")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int [] CopyArray (int [] arr)
+{
+    int[] newArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+Console.WriteLine ("Введите число элементов");
+int elements = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите минимальное число в массиве");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите максимальное число в массиве");
+int maximum = Convert.ToInt32(Console.ReadLine());
+int[] oldArray = CreateArrayRndInt(elements, minimum, maximum);
+
+Console.Write("[");
+PrintArray(oldArray);
+Console.WriteLine("]");
+
+int[] copyArr = CopyArray (oldArray);
+
+Console.Write("[");
+PrintArray(copyArr);
+Console.WriteLine("]");
