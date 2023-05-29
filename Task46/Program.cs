ab5fb7833c7d793de6b9bfefc830f7e8e40859f0
Misha <misha.hanzhin@gmail.com>
2023-05-29 22:59:58 +0500

    Вывод двуметрного массива со случайными числами

---
+++ Task46/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+int[,] CreatewMatrixRndInt (int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength (0); i ++) // rows = arr.GetLength (0)
+    {
+        for (int j = 0; j < matrix.GetLength (1); j++) // columns = arr.GetLength (1)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1); // [1;10)]
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength (0); i ++) 
+    {
+        //Console.Write ("|");
+        for (int j = 0; j < matrix.GetLength (1); j++) 
+        {
+            Console.Write($"{matrix[i, j], 5} "); // 5 - форматирвоание отступа
+        }
+        //Console.Write ("|");
+    Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreatewMatrixRndInt (3, 4, -100, 100); //лучше пользовательский ввод
+PrintMatrix(array2d);
