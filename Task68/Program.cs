8840e680bde362ca5a4238b5bbd6ce54dbadd583
Misha <misha.hanzhin@gmail.com>
2023-06-08 21:48:04 +0500

    Функция Аккермана

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -20,6 +20,6 @@ else Console.WriteLine("Числа M и N должны быть больше 0")
 int Ackermann (int m, int n)
 {
     if (m == 0) return n+1;
-    else if (n == 0) return Ackermann(m-1, 1);
-    else return Ackermann(m-1, Ackermann(m, n-1));
+    if (n == 0) return Ackermann(m-1, 1);
+    return Ackermann(m-1, Ackermann(m, n-1));
 }
